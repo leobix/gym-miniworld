01c2568f4775eb9072ca8b4e35560494ecc3fc88
dvVenuto <dv.venuto@gmail.com>
2019-05-14 15:48:01 -0400

    Fix psc add in agent

--- pytorch-a2c-ppo-acktr/main_agent.py
+++ pytorch-a2c-ppo-acktr/main_agent.py
@@ -239,6 +239,8 @@ def main():
 
             # If done then clean the history of observations.
             masks = torch.FloatTensor([[0.0] if done_ else [1.0] for done_ in done])
+            psc_add =torch.FloatTensor([0.0])
+
             rollouts.insert(obs, recurrent_hidden_states, action, action_log_prob, value, reward, masks, psc=psc_add)
 
         with torch.no_grad():
